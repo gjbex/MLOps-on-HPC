126b093a25c7cf00083f1c667fcce5558714cda2
Geert Jan Bex <geertjan.bex@uhasselt.be>
2025-08-01 14:40:37 +0200

    Make experiment a bit more interesting

--- source-code/dvc/experiment-management_r/src/experiment.R
+++ source-code/dvc/experiment-management_r/src/experiment.R
@@ -1,28 +1,31 @@
+#!/usr/bin/env Rscript
+
+library(optparse)
 library(reticulate)
 
-# Load the Python module
+option_list <- list(
+  make_option(c("--a"), type = "double", default = 1.0, help = "Parameter a"),
+  make_option(c("--t_max"), type = "integer", default = 10, help = "Maximum number of time steps")
+)
+parser <- OptionParser(option_list = option_list, description = "Run an experiment with specified parameters.")
+opts <- parse_args(parser)
+
 py_module_available("dvclive") || stop("Python module 'dvclive' is not available")
 dvclive <- import("dvclive")
 
-# Initialize dvclive
-live <- dvclive$Live(report='html')
+live <- dvclive$Live(report = "html")
 
-# delta is a command line parameter, initialize and log it
-arguments <- commandArgs(trailingOnly = TRUE)
-if (length(arguments) == 0) {
-  stop("No command line arguments provided. Please provide a value for 'delta'.")
+compute <- function(t, a) {
+  a * t^2
 }
-delta <- as.numeric(arguments[1])
-live$log_param("delta", delta)
 
-# Simulate a process and log metrics
-current_score <- 0.0
-live$log_metric("score", current_score)
-live$next_step()
-for(step in 1:100) {
-  current_score <- current_score + delta
-  live$log_metric("score", current_score)
+live$log_param("a", opts$a)
+live$log_param("t_max", opts$t_max)
+
+for (t in seq(0, opts$t_max)) {
+  result <- compute(t, opts$a)
+  live$log_metric("result", result)
   live$next_step()
 }
 
-live$end()  # finalize logs
+live$end()
